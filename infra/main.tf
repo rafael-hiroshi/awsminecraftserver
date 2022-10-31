f2100d8429dcc6e550edd946c08afbb1c2aca6d3
Rafael Hiroshi <rafaelhkp@gmail.com>
2022-10-30 22:03:11 -0300

    Add EC2 Instance

--- infra/main.tf
+++ infra/main.tf
@@ -11,20 +11,62 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_iam_role_policy_attachment" "aws_iam_role_attach"{
-  for_each = toset([
-    aws_iam_policy.s3_bucket.arn,
-    aws_iam_policy.kms_policy.arn,
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-    "arn:aws:iam::aws:policy/CloudWatchAgentAdminPolicy"
-  ])
+resource "aws_instance" "minecraft_server" {
+  ami           = var.ec2_ami
+  instance_type = "t3.micro"
+  hibernation = true
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  security_groups = [aws_security_group.allow_game_traffic.name]
 
-  role       = aws_iam_role.ec2_instance_role.name
-  policy_arn = each.value
+  root_block_device {
+    delete_on_termination = false
+    encrypted = true
+    iops = 3000
+    throughput = 125
+    kms_key_id = var.kms_key_arn
+    volume_size = 12
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Minecraft Server"
+  }
 }
 
-resource "aws_iam_role" "ec2_instance_role" {
-  name = "MinecraftServerEC2InstanceRole"
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "MinecraftServerEC2InstanceProfile"
+  role = aws_iam_role.ec2_service_role.name
+}
+
+resource "aws_security_group" "allow_game_traffic" {
+  name        = "Minecraft Server Instance SG"
+  description = "Allow default minecraft game inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port        = 25565
+    to_port          = 25565
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "aws_iam_role_attach"{
+  role       = aws_iam_role.ec2_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role" "ec2_service_role" {
+  name = "MinecraftServerEC2ServiceRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -51,7 +93,7 @@ resource "aws_iam_policy" "s3_bucket" {
         "Action": [
           "s3:ListBucket"
         ],
-        "Resource": var.s3_bucket_name
+        "Resource": var.s3_bucket_arn
       },
       {
         "Effect": "Allow",
@@ -59,7 +101,7 @@ resource "aws_iam_policy" "s3_bucket" {
           "s3:PutObject",
           "s3:GetObject"
         ],
-        "Resource": "${var.s3_bucket_name}/*"
+        "Resource": "${var.s3_bucket_arn}/*"
       }
     ]
   })
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,15 @@
-variable "s3_bucket_name" {
+variable "s3_bucket_arn" {
+  type = string
+}
+
+variable "ec2_ami" {
+  type = string
+}
+
+variable "kms_key_arn" {
+  type = string
+}
+
+variable "vpc_id" {
   type = string
 }
