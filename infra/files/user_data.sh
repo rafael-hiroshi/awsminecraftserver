fba1882602fcf1b1d4ee4a2dc5b1495c4ffb7d26
Rafael Hiroshi <rafaelhkp@gmail.com>
2022-12-03 20:47:04 -0300

    Fetch server data from S3

--- infra/files/user_data.sh
+++ infra/files/user_data.sh
@@ -7,28 +7,39 @@ yum install java-17-amazon-corretto.x86_64 \
 {
   echo "MINECRAFT_VERSION=1.19.2"
   echo "S3_BUCKET=hiroshi-minecraft-servers-data"
-  echo "SERVER_BASE_PATH=/var/www/minecraft"
+  echo "BASE_PATH=/var/www/minecraft"
+  echo "SERVER_DIRECTORY=/var/www/minecraft/server"
 } >> /etc/environment
 source /etc/environment
 
-mkdir -p "$SERVER_BASE_PATH"
-cd "$SERVER_BASE_PATH" || exit
+mkdir -p "$SERVER_DIRECTORY"
+cd "$BASE_PATH" || exit
 
 cat >start_server.sh <<__EOF__
 #!/bin/bash -x
 
-java -Xms1024M -Xmx3584M -jar server.jar nogui
+java -Xms1024M -Xmx3584M -jar $SERVER_DIRECTORY/server.jar nogui
 __EOF__
 
 cat >s3_backup.sh <<__EOF__
 #!/bin/bash -x
 
-cd $SERVER_BASE_PATH
+cd $SERVER_DIRECTORY
 zip -r minecraft_server_$MINECRAFT_VERSION.zip .
 aws s3 cp minecraft_server_$MINECRAFT_VERSION.zip s3://hiroshi-minecraft-servers-data/1.19.2/minecraft_server_$MINECRAFT_VERSION.zip
 __EOF__
 
+cat >s3_fetch.sh <<__EOF__
+#!/bin/bash -x
+
+cd $SERVER_DIRECTORY
+aws s3 cp s3://hiroshi-minecraft-servers-data/1.19.2/minecraft_server_1.19.2.zip .
+unzip minecraft_server_1.19.2.zip
+rm -fv minecraft_server_1.19.2.zip
+__EOF__
+
 chmod +x start_server.sh
 chmod +x s3_backup.sh
+chmod +x s3_fetch.sh
 
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-EC2MinecraftServerCWAgent -s
--- src/s3_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/ec2-user/server/
-zip -r minecraft_vanilla_server_1.19.2.zip .
-aws s3 cp minecraft_vanilla_server_1.19.2.zip s3://hiroshi-minecraft-servers-data/1.19.2/minecraft_vanilla_server_1.19.2.zip
